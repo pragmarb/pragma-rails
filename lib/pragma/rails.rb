8ef28a568247c1a9fbb0fa5969cc7c0815f2deca
Alessandro Desantis <desa.alessandro@gmail.com>
2016-12-30 13:01:11 +0100

    Test Pragma::Rails::Controller

--- lib/pragma/rails.rb
+++ lib/pragma/rails.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
+require 'pragma'
+
 require 'pragma/rails/version'
+require 'pragma/rails/controller'
+require 'pragma/rails/resource_controller'
 
 module Pragma
   # Ruby on Rails integration for the Pragma architecture.
---
+++ spec/pragma/rails/controller_spec.rb
@@ -0,0 +1,52 @@
+RSpec.describe Pragma::Rails::Controller do
+  let(:controller_klass) do
+    Class.new do
+      OPERATION_KLASS = Class.new(Pragma::Operation::Base) do
+        def call
+          headers['X-Foo'] = params[:foo]
+          respond_with status: :created, resource: { foo: params[:foo] }
+        end
+      end
+
+      attr_reader :render_args
+
+      def run_operation
+        run OPERATION_KLASS
+      end
+
+      def response
+        @response ||= OpenStruct.new(headers: {})
+      end
+
+      def render(*args)
+        @render_args = args
+      end
+
+      protected
+
+      def operation_params
+        { foo: 'bar' }
+      end
+    end.tap do |klass|
+      klass.include described_class
+    end
+  end
+
+  subject do
+    controller_klass.new.tap do |controller|
+      controller.run_operation
+    end
+  end
+
+  it 'responds with the headers from the operation' do
+    expect(subject.response.headers).to eq('X-Foo' => 'bar')
+  end
+
+  it 'responds with the status code from the operation' do
+    expect(subject.render_args.first[:status]).to eq(:created)
+  end
+
+  it 'responds with the resource from the operation' do
+    expect(subject.render_args.first[:json]).to eq(foo: 'bar')
+  end
+end
