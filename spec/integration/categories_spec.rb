a24e020100b41550fa099a445b6788caae35e251
Alessandro Desantis <desa.alessandro@gmail.com>
2017-04-03 16:45:57 +0200

    Add integration tests for /categories

--- spec/integration/categories_spec.rb
+++ spec/integration/categories_spec.rb
@@ -11,6 +11,13 @@ RSpec.describe '/api/v1/categories' do
       ])
     end
 
-    it 'adds pagination info to the headers'
+    it 'adds pagination info to the headers' do
+      subject.call
+      expect(last_response.headers).to match(a_hash_including(
+        'Total' => 1,
+        'Per-Page' => 30,
+        'Page' => 1
+      ))
+    end
   end
 end
