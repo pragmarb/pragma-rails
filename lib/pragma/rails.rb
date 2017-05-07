03fd483b9978c636f2a969298f3bd67521d61d70
Alessandro Desantis <desa.alessandro@gmail.com>
2017-05-07 15:19:35 +0200

    Remove outdated hooks for default Index

--- lib/pragma/rails.rb
+++ lib/pragma/rails.rb
@@ -6,7 +6,6 @@ require 'rails'
 require 'pragma/rails/version'
 require 'pragma/rails/controller'
 require 'pragma/rails/resource_controller'
-require 'pragma/rails/hooks'
 
 module Pragma
   # Ruby on Rails integration for the Pragma architecture.
